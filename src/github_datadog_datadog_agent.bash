eccf9bb1e4676604b59ccca11b438e4e25ffe542
Florent Clarret <florent.clarret@gmail.com>
2023-07-13 11:08:25 +0200

    feat(github): add a shortcut to datadog-agent

---
+++ src/github_datadog_datadog_agent.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open the Datadog's datadog-agent page
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.packageName GitHub
+# @raycast.icon images/github-logo.png
+# @raycast.iconDark images/github-logo-iconDark.png
+
+open https://github.com/DataDog/datadog-agent
