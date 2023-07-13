4a40ddc53936c943f621d16e29cbb0085c5de008
Florent Clarret <florent.clarret@gmail.com>
2023-07-13 11:06:59 +0200

    feat(github): add a shortcut to integrations-extras

---
+++ src/github_datadog_integrations_extras.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open the Datadog's integrations-core page
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.packageName GitHub
+# @raycast.icon images/github-logo.png
+# @raycast.iconDark images/github-logo-iconDark.png
+
+open https://github.com/DataDog/integrations-core
