74afb644bfcf6ae67bbaab6a12c7d472a2a4e55a
Florent Clarret <florent.clarret@gmail.com>
2026-04-06 07:28:53 +0200

    feat(cyberchef): add docs

--- src/cyberchef.bash
+++ src/cyberchef.bash
@@ -5,4 +5,9 @@
 # @raycast.title Open CyberChef
 # @raycast.mode silent
 
+# Documentation:
+# @raycast.author Florent Clarret
+# @raycast.authorURL https://github.com/FlorentClarret
+# @raycast.description Start a cyberchef server and open it
+
 /Users/florent.clarret/.scripts/cyberchef.sh
